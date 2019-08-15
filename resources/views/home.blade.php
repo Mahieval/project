1c0b662b8dff176c50b2b426a103205e22f64184
Mahieval <mahievalfaris@gmail.com>
2019-08-15 10:36:48 +0700

    project pertama

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('adminlte::page')
+
+@section('title', 'PRO_LTE')
+
+@section('content_header')
+    <h1 align="center">PUNTEN SLUR</h1>
+@stop
+
+@section('content')
+    <p align="center">Ges Login Cuk!</p>
+@stop
